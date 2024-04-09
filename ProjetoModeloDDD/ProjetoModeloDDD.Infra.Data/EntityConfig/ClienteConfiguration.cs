353237bb6c9e5ab67190eb8d21fa0b16d02f2c28
Matheus Pires <matheuswith51@hotmail.com>
2024-04-08 22:46:23 -0300

    reupando projeto de estudo DDD

---
+++ ProjetoModeloDDD/ProjetoModeloDDD.Infra.Data/EntityConfig/ClienteConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using ProjetoModeloDDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoModeloDDD.Infra.Data.EntityConfig
+{
+    public class ClienteConfiguration : EntityTypeConfiguration<Cliente>
+    {
+        public ClienteConfiguration()
+        {
+            HasKey(C => C.ClienteId);
+
+            Property(c => c.Nome)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            Property(c => c.Sobrenome)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            Property(c => c.Email)
+                .IsRequired();
+        }
+
+    }
+}
